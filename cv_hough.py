5bc2c46858624e051b237303d9c841c072c1d2a9
theCow61 <zanesalti@gmail.com>
2026-04-05 16:43:03 -0500

    stf

--- cv_hough.py
+++ cv_hough.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img = np.fromfile("C:\\Users\zfsalti\\488\MP-3\images\BIMG_010.BIN", dtype=np.uint16).reshape((1080, 1920))
+img = np.fromfile("C:\\Users\zfsalti\\488\MP-3\images\BIMG_000.BIN", dtype=np.uint16).reshape((1080, 1920))
 
 
 raw_pixels = np.frombuffer(img, dtype=np.uint16)
@@ -23,5 +23,19 @@ for i in circles[0,:]:
     cv.circle(blurred_img, (i[0], i[1]), i[2], 0, 2)
     cv.circle(blurred_img, (i[0], i[1]), 2, 0, 2)
 
-plt.imshow(blurred_img, cmap="gray", vmin=0, vmax=255)
+accumulator = np.zeros((1080, 1920), dtype=np.float32)
+
+centers = circles[0, :, :2]
+
+for (x, y) in centers:
+    cv.circle(accumulator, (x,y), radius=30, color=1, thickness=-1)
+
+accumulator = cv.GaussianBlur(accumulator, (51, 51), 15)
+
+_, _, _, max_loc = cv.minMaxLoc(accumulator)
+print(max_loc)
+
+# plt.imshow(blurred_img, cmap="gray", vmin=0, vmax=255)
+plt.imshow(accumulator)
+
 plt.show()
