ee7e4e0f633d797378d1b3f7a7fe5724d2472900
Zane Salti <zanesalti@gmail.com>
2026-04-06 20:04:32 -0500

    cv

--- bullseye_track/main.cpp
+++ bullseye_track/main.cpp
@@ -7,7 +7,8 @@
 
 uint16_t framebuffer[1920*1080];
 
-uint8_t downscaled_gray_img[480*270];
+uint8_t downscaled_gray_buffer[480*270];
+uint8_t blurred_buffer[480*270];
 
 void update_framebuffer() {
     std::ifstream file("../images/BIMG_000.BIN", std::ios::binary);
@@ -20,7 +21,7 @@ void downscale_and_gray() {
     for (int r = 0; r < 270; r++) {
         const uint16_t* row = framebuffer + r * 4 * 1920;
         for (int c = 0; c < 480; c++) {
-            downscaled_gray_img[r * 480 + c] = static_cast<uint8_t>(row[c * 4]);
+            downscaled_gray_buffer[r * 480 + c] = static_cast<uint8_t>(row[c * 4]);
         }
     }
 }
@@ -30,15 +31,19 @@ int main() {
     std::cout << "Starting" << std::endl;
     while (1) {
 
-        std::cout << "test1" << std::endl;
         update_framebuffer();
         downscale_and_gray();
 
-        cv::Mat img(270, 480, CV_8UC1, downscaled_gray_img);
+        cv::Mat img(270, 480, CV_8UC1, downscaled_gray_buffer);
+        cv::Mat blurred_img(270, 480, CV_8UC1, blurred_buffer);
 
-        bool ok = cv::imwrite("test.png", img);
-        std::cout << "imwrite: " << ok << std::endl;
+        cv::GaussianBlur(img, blurred_img, cv::Size(3, 3), 1);
+        cv::threshold(blurred_img, img, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
 
+
+
+        cv::imwrite("test.png", img);
+        return 1;
     }
 
 }
